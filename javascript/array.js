04c5edaeea1a9482005df5cf7465fd7b7e12047e
Huy Doanx <huycntt03@gmail.com>
2021-10-27 17:28:05 +0700

    BT WEB SUMMER 2021

--- javascript/array.js
+++ javascript/array.js
@@ -17,45 +17,45 @@ var languages = [
     'Ruby',
 ];
 //1. toString biến đổi số thành kiểu string
-console.log(languages.toString())
+// console.log(languages.toString())
 
 //2. join tương tự toString nhưng chèn thêm được phần tử
-console.log(languages.join(' '))
+// console.log(languages.join(' '))
 
 //3. pop xóa element cuối mảng và trả về phần tử đã xóa
-console.log(languages.pop())
-console.log(languages.pop())
-console.log(languages)
+// console.log(languages.pop())
+// console.log(languages.pop())
+// console.log(languages)
 
 //4. push thêm các phần tử vào cuối mảng và trả về độ dài mới của mảng
-console.log(languages.push('Dart', 'Java'))
-console.log(languages)
+// console.log(languages.push('Dart', 'Java'))
+// console.log(languages)
 
 //5. shift xóa element ở đầu mảng và trả về phẩn tử đã xóa
-console.log(languages.shift())
-console.log(languages)
+// console.log(languages.shift())
+// console.log(languages)
 
 //6. unshift thêm phần tử vào đầu mảng và trả về độ dài mới
-console.log(languages.unshift('Dart','Java'))
-console.log(languages)
+// console.log(languages.unshift('Dart','Java'))
+// console.log(languages)
 
 //7. splicing (splice()) xóa , cắt , chèn phần tử mới vào mảng
 // languages.splice(vị trí đặt con trỏ, vị trí cần xóa, phần tử cần thêm)
 // xóa
-languages.splice(1 ,1 )
+// languages.splice(1 ,1 )
 // chèn
-languages.splice(1, 0, 'Dart')
-console.log(languages)
+// languages.splice(1, 0, 'Dart')
+// console.log(languages)
 
 //8. concat nối array
-var languages2 = [
-    'Dart',
-    'Java'
-];
-console.log(languages.concat(languages2))
-console.log(languages2.concat(languages))
+// var languages2 = [
+//     'Dart',
+//     'Java'
+// ];
+// console.log(languages.concat(languages2))
+// console.log(languages2.concat(languages))
 
 // 9. slicing (slice()) cắt 1 hoặc vài element và trả về các element đó
  //slice(vị trí bắt đầu cắt, vị trí dừng cắt)
-console.log(languages.slice(1,3))
-console.log(languages)
+// console.log(languages.slice(1,3))
+// console.log(languages)
--- javascript/function.js
+++ javascript/function.js
@@ -2,7 +2,7 @@
 function showDialog() {
     alert('Hi xin chao cac bạn ');
 }
-showDialog();
+// showDialog();
 
 function writeLog(message, message2) {
     if (message)
@@ -10,20 +10,20 @@ function writeLog(message, message2) {
     if (message2)
         console.log(message2)
 }
-writeLog('Test', 'Test_2');
+// writeLog('Test', 'Test_2');
 
 // arguments có viết nhiều element cùng lúc mà k cần đặt biến
 function writeArg() {
     console.log(arguments)
 }
-writeArg('Arg1', 'Arg2', 'Arg3')
+// writeArg('Arg1', 'Arg2', 'Arg3')
 
 function writeFor() {
     for (var param of arguments) {
         console.log(param)
     }
 }
-writeFor('For1', 'For2', 'For3')
+// writeFor('For1', 'For2', 'For3')
 
 function writeFor2() {
     var myString = '';
@@ -32,7 +32,7 @@ function writeFor2() {
     }
     console.log(myString);
 }
-writeFor2('For1', 'For2', 'For3')
+// writeFor2('For1', 'For2', 'For3')
 
 
 // return trong hàm
@@ -40,9 +40,15 @@ function cong(a, b) {
     return a + b;
 }
 var sum = cong(2.5, 8);
-console.log(sum);
+// console.log(sum);
 
-// Các loại function
+function ShowMessage(){
+    function ShowMessage2(){
+        console.log('MESS');
+    }
+    ShowMessage2();
+}
 
+// ShowMessage();
 
 
